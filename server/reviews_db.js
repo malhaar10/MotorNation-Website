d21fdb31f5f2b9c39308748f233ad44cc667ea4a
malhaar10 <123005720+malhaar10@users.noreply.github.com>
2025-10-17 17:47:20 +0530

    Use identifiable database environment variables: REVIEWS_DATABASE and NEWS_DATABASE

--- server/reviews_db.js
+++ server/reviews_db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
   host: process.env.PGHOST,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE, // Use environment variable like news_db.js
+  database: process.env.REVIEWS_DATABASE,
   port: process.env.PGPORT,
 });
 
