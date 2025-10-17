e983950bf1022a9494c8ff9de53ff37640734894
malhaar10 <123005720+malhaar10@users.noreply.github.com>
2025-10-17 17:37:49 +0530

    Fix reviews database connection to use environment variable

--- server/reviews_db.js
+++ server/reviews_db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
   host: process.env.PGHOST,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
-  database: 'reviews', // explicitly use reviews database
+  database: process.env.PGDATABASE, // Use environment variable like news_db.js
   port: process.env.PGPORT,
 });
 
