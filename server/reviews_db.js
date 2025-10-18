5724dd283e9fa7d1541c87a514f48b400e571140
malhaar10 <123005720+malhaar10@users.noreply.github.com>
2025-10-18 14:18:05 +0530

    fix: Update database configuration in news_db and reviews_db to use correct environment variables

--- server/reviews_db.js
+++ server/reviews_db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
   host: process.env.PGHOST,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
-  database: process.env.REVIEWS_DATABASE,
+  database: 'reviews', // explicitly use reviews database
   port: process.env.PGPORT,
 });
 
